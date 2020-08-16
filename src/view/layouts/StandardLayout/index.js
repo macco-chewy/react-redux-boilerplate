48611e5c8da70707ed0fc6a522b9e72b1323a0df
Mike Cherry <mcherry@zamphear.net>
2020-08-15 22:09:19 -0700

    Added navigation example

--- src/view/layouts/StandardLayout/index.js
+++ src/view/layouts/StandardLayout/index.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './styles.module.css';
 
 export default (props) => {
   return (
     <div className={styles.root}>
-      <div>header</div>
-      <div>{props.children}</div>
-      <div>footer</div>
+      <div className={styles.header}>
+        <div className={styles.row1}>
+          <div className={styles.logo}>Logo</div>
+          <div className={styles.details}></div>
+        </div>
+        <ul className={styles.menu}>
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+          <li>
+            <Link to="/page1">Page 1</Link>
+          </li>
+          <li>
+            <Link to="/page2">Page 2</Link>
+          </li>
+          <li>
+            <Link to="/page3">Page 3</Link>
+          </li>
+        </ul>
+      </div>
+      <div className={styles.body}>
+        <div className={styles.bodyContents}>{props.children}</div>
+      </div>
+      <div className={styles.footer}></div>
     </div>
   );
 };
